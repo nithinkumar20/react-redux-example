36a9363ac8baaa4e50233017ee725c0ff5a2af5d
nithinkumar20 <nithinm.015@gmail.com>
2019-04-23 15:49:57 +0530

    Updates

---
+++ precache-manifest.e458ad2979b9ef69ef043396cddd1d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "968ad9f1b55c79519d11",
+    "url": "/react-redux-example/static/js/runtime~main.968ad9f1.js"
+  },
+  {
+    "revision": "a0a2a53be9c55f2af5d4",
+    "url": "/react-redux-example/static/js/main.a0a2a53b.chunk.js"
+  },
+  {
+    "revision": "4752e5efd9f44591f6ed",
+    "url": "/react-redux-example/static/js/2.4752e5ef.chunk.js"
+  },
+  {
+    "revision": "a0a2a53be9c55f2af5d4",
+    "url": "/react-redux-example/static/css/main.d52ee69e.chunk.css"
+  },
+  {
+    "revision": "d0f903e70ef78bfc067df197b59ac00a",
+    "url": "/react-redux-example/index.html"
+  }
+];
